12d8e0de800a67ba2ad1acb0ab59335bc9567db2
Alex Lee <ahjlee712@gmail.com>
2021-02-26 16:51:11 -0500

    Created components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Product from './product-overview.jsx';
+import RelatedProduct from './related-products.jsx';
+import Questions from './questions.jsx';
+import Reviews from './reviews.jsx';
 
-const App = () => (
-  <div>
-    <h1>Hello</h1>
-  </div>
-);
+class App extends React.Component{
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        <Product />
+        <RelatedProduct />
+        <Questions />
+        <Reviews />
+     </div>
+    )
+  }
+}
 
 export default App;
---
+++ client/components/questions.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Questions = () => (
+  <div>
+    <h1>Hello</h1>
+  </div>
+);
+
+export default Questions;
