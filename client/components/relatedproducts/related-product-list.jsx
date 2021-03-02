b63044e35002770ee8f1d613b18cfb62349012ee
Alex Lee <ahjlee712@gmail.com>
2021-03-02 02:18:06 -0500

    Successfully got data to render into the console

--- client/components/relatedproducts/related-product-list.jsx
+++ client/components/relatedproducts/related-product-list.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import RelatedProductCard from './related-product-card.jsx';
 import axios from 'axios';
 
 const RelatedList = () => {
 
-  // axios.get('/')
-  //   .then(res => console.log(res.data)
-  //   .catch(err => console.log(err))
+  //use hooks to render the intial set of data from the API
+  const [data, setData] = useState([]);
+
+  //this will do a componentDidMount-like functionality
+  useEffect(() => {
+    const url = '/proxy/api/fec2/hratx/products';
+    axios.get(url)
+      .then(res => console.log(res.data))
+      .catch(err => console.log('i get error here',err))
+  }, [])
 
   return (
     <div className = 'related-list'>
-      <h1>Hello</h1>
+      <h1>Related items</h1>
     </div>
   )
 };
-
-//
 
 export default RelatedList;
--- server.js
+++ server.js
@@ -1,32 +1,47 @@
 const express = require('express');
-const port = 3000;
+const port = 3001;
 const path = require('path');
-const token = require('./token.js');
+const TOKEN = require('./token.js');
 const axios = require('axios');
+const proxy = require('express-http-proxy');
 const bodyparser = require('body-parser');
 const app = express();
 
-let options = {
-  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hratx/products',
-  headers: {
-    'User-Agent': 'request',
-    'Authorization': token
-  }
-};
+// let options = {
+//   url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hratx',
+//   headers: {
+//     'Content-Type': 'application/json;charset=UTF-8',
+//     'User-Agent': 'request',
+//     "Access-Control-Allow-Origin": "*",
+//     'Authorization': token
+//   }
+// };
 
 //make a request here to specific things you're looking for
 
-app.use(bodyparser.json());
+// app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, '/dist')));
 
-app.get('/', (req, res) => {
-  axios.get(options.url, options.headers)
-    .then(response => {
-      console.log(response);
-      res.send(response)
-    })
-    .catch(err => res.status(500).send(err))
-})
+app.use('/proxy',
+  proxy('https://app-hrsei-api.herokuapp.com/', {
+  proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+    proxyReqOpts.headers['Authorization'] = TOKEN;
+    // console.log(srcReq);
+    // console.log(proxyReqOpts.headers);
+      proxyReqOpts.method = 'GET';
+      return proxyReqOpts;
+    }
+  })
+)
+
+// app.get('/products', (req, res) => {
+//   axios.get(options.url, options.headers)
+//     .then(response => {
+//       console.log('it reaches here');
+//       res.send(response)
+//     })
+//     .catch(err => res.status(500).send(err))
+// })
 
 app.listen(port, () => {
   console.log(`You're listening on port ${port}`)
