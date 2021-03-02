3143b7b6b1c9f675b891d127a38980bedbe9b35e
Brandon Harden <bmh0013@gmail.com>
2021-03-02 14:06:50 -0600

    Created the review card component

---
+++ client/components/Reviews/reviewCard.jsx
@@ -0,0 +1,26 @@
+import React, { useState,useEffect } from 'react';
+var moment = require('moment');
+
+const ReviewCard = (props) => {
+  let date = moment(props.review.date, 'YYYY-MM-DD').format('MMMM D, YYYY');
+  console.log('data:', props.data);
+  console.log('review:', props.review)
+
+  return (
+    <div className='review-card'>
+      <span className="rating">Rating: {props.review.rating}</span>
+      <span className="user_date">{props.review.reviewer_name} | {date}</span>
+      <h5>{props.review.body}</h5>
+      <p>
+        {props.review.summary}
+      </p>
+      {props.review.response && <p><u>Response:</u> {props.review.response}</p>}
+      <span className='helpful'>
+        Helpful? Yes ({props.review.helpfulness}) | Report
+      </span>
+      {props.review.recommend && <span className='recommend'><u>I recommend this product!</u></span>}
+    </div>
+    )
+};
+
+export default ReviewCard;
