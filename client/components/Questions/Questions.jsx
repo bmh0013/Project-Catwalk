c6580b1cfb179002710d3357681f8189d32e4946
Michael Kolarik <mikekolarik@gmail.com>
2021-03-03 11:01:22 -0600

    Implement load more/show less questions

--- client/components/Questions/Questions.jsx
+++ client/components/Questions/Questions.jsx
@@ -7,6 +7,8 @@ import sampleData from './sampleData.js';
 const Questions = () => {
   const [productId, setProductId] = useState(21111);
   const [data, setData] = useState([]);
+  const [questionsToShow, setQuestionsToShow] = useState(4);
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     loadData(productId);
@@ -17,14 +19,26 @@ const Questions = () => {
       .then(receivedData => setData(receivedData.results.sort((a, b) => (a.helpfulness > b.helpfulness) ? -1 : 1)));
   };
 
+  const showMore = () => {
+    expanded ? setQuestionsToShow(4) : setQuestionsToShow(data.length);
+    setExpanded(!expanded);
+  }
+
   return (
     <div>
       <h1>Questions and Answers</h1>
       <div>
         <QuestionSearch />
-        {data.map(q => (
+        {data.slice(0, questionsToShow).map(q => (
           <Question question={q} key={q.question_id} />
         ))}
+        <a className="btn btn-primary" onClick={showMore}>
+          {expanded ? (
+            <span>SHOW LESS QUESTIONS</span>
+          ) : (
+            <span>LOAD MORE QUESTIONS</span>
+          )}
+        </a>
       </div>
     </div>
   );
