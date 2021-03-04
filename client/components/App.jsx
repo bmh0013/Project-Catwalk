5553ef038a1f05e04b5aec49ed3b97b55072d6c6
Alex Lee <ahjlee712@gmail.com>
2021-03-04 14:13:16 -0500

    Began to add current item to modal. Deleted carousel file

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -13,11 +13,11 @@ class App extends React.Component{
   render() {
     return(
       <div>
-        <Overview />
+        {/* <Overview /> */}
         <RelatedList />
         <YourOutfitList />
-        <Questions />
-        <Reviews />
+        {/* <Questions />
+        <Reviews /> */}
      </div>
     )
   }
--- client/components/relatedproducts/modalDetails.jsx
+++ client/components/relatedproducts/modalDetails.jsx
@@ -1,4 +1,5 @@
 //this will take care of showing all the card details when a user clicks on a click
+//this compares the clicked product to the current item in the overview
 
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
@@ -7,6 +8,7 @@ const ModalDetails = ({currentProductId, name, category, price}) => {
 
   const [currentProduct, setCurrentProduct] = useState([]);
 
+  //should run whenever a new current product id has changed
   useEffect(() => {
     const url = `/proxy/api/fec2/hratx/products/${currentProductId}`;
     axios.get(url)
@@ -22,39 +24,13 @@ const ModalDetails = ({currentProductId, name, category, price}) => {
     <div>
       <h1>COMPARING</h1>
       <h2>{currentProductId}</h2>
-      <p>{name}<span className = 'comparison'>Name</span></p>
-      <p>{category}<span className = 'comparison'>Category</span></p>
-      <p>{price}<span className = 'comparison'>Price</span></p>
+      <p>{name}<span className = 'comparison'>Name</span>{currentProduct.name}</p>
+      <p>{category}<span className = 'comparison'>Category</span>{currentProduct.category}</p>
+      <p>{price}<span className = 'comparison'>Price</span>{currentProduct.default_price}</p>
+      <p><span className = 'comparison'>Number of Features</span>{currentProduct.features.length}</p>
 
     </div>
   )
 };
 
 export default ModalDetails;
-
-{/* <div className = 'modal-content'>
-        <p>hello</p>
-      </div> */}
-
-
-
-      // console.log('does it reach here?');
-      // const [openModal, setOpenModal] = useState(false);
-
-      // const toggleModal = () => {
-      //   setOpenModal(!openModal);
-      // }
-
-    //   <div className = 'card-modal'>
-    //   <button onClick = {toggleModal}>Open Modal</button>
-    //   <Modal
-    //     isOpen = {openModal}
-    //     onRequestClose = {toggleModal}
-    //     contentLabel = 'My Dialog'
-    //     className = 'mymodal'
-    //     overlayClassName = 'myoverlay'
-    //     closeTimeoutMS = {500}
-    //   >
-    //     <div>My modal dialog</div>
-    //   </Modal>
-    // </div>
