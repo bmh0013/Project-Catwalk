4122a18b4c759a66e0f8d89fec5b1966ec44d444
Brandon Harden <bmh0013@gmail.com>
2021-03-06 12:15:36 -0600

    created file to handle all api requests

---
+++ api.js
@@ -0,0 +1,77 @@
+import { TOKEN } from './token.js';
+const axios = require('axios').default;
+
+
+// Handles all get requests, requires a route and a params
+function handleGetRequests(route, params) {
+  let headers;
+  if (params) {
+    headers = {
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hratx/${route}`,
+      headers: {
+        Authorization: TOKEN
+      },
+      params: params
+    }
+  } else {
+    headers = {
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hratx/${route}`,
+      headers: {
+        Authorization: TOKEN
+      }
+    }
+  }
+  return axios(headers)
+}
+
+function getAllProducts() {
+  return handleGetRequests('products')
+}
+
+function getProduct(product_id) {
+  return handleGetRequests(`products/${product_id}`)
+}
+
+function getReviewCards(params) {
+  return handleGetRequests('reviews', params)
+}
+
+function getMetadata(params) {
+  return handleGetRequests('reviews/meta', params)
+}
+
+function getQuestions(params) {
+  return handleGetRequests('qa/questions/', params)
+}
+
+function getAnswers(question_id, params) {
+  return handleGetRequests(`qa/questions/${question_id}/answers`, params)
+}
+
+// Handles all put requests, requires a route and params object
+function handlePostRequests(route, params) {
+  let headers = {
+    method: 'post',
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hratx/${route}`,
+    headers: {
+      Authorization: TOKEN
+    },
+    params: params
+  };
+  return axios(headers)
+}
+
+function postReview(params) {
+  return handlePostRequests('reviews', params)
+}
+
+export default {
+  getAllProducts,
+  getProduct,
+  getReviewCards,
+  getMetadata,
+  getQuestions,
+  getAnswers
+}
