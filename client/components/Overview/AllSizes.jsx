72b21692225e8e1c41fce114fd6206b7e2960964
lukas-schweter <schweterlukas@gmail.com>
2021-03-11 10:41:34 -0600

    fixed some bugs with style selector & started size selector

---
+++ client/components/Overview/AllSizes.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+
+function AllSizes({ sizes }) {
+  console.log('SIZES: ', sizes);
+  let sizeArr = [];
+  for (let size in sizes) {
+    sizeArr.push([size, sizes[size].quantity, sizes[size].size]);
+    console.log(sizeArr);
+  }
+
+  if (sizeArr.length > 0) {
+    return (
+      <div>
+        {sizeArr.map(size => {
+          console.log(size);
+          return (
+            <option key={size[0]} quantity={size[1]} size={size[2]}>{size[2]}</option>
+          )
+        })}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <option> OUT OF STOCK </option>
+      </div>
+    )
+  }
+
+}
+
+export{
+  AllSizes
+}
